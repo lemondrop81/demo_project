d76bf4080bfe1d7a7d8a5e5042fdeeabe588d6f2
Leum <you@example.com>
2021-06-14 09:34:36 -0500

    Resourceful Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,31 @@
 Rails.application.routes.draw do
 
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
-  get 'pages/edit'
-  get 'pages/delete'
-  get 'subjects/index'
-  get 'subjects/show'
-  get 'subjects/new'
-  get 'subjects/edit'
-  get 'subjects/delete'
+  resources :subjects do
+    member do
+      get :delete
+    end
+
+  end
+
+  resources :pages do
+    member do
+      get :delete
+    end
+
+  end
+
+  #get 'pages/index'
+  #get 'pages/show'
+  #get 'pages/new'
+  #get 'pages/edit'
+  #get 'pages/delete'
+
+  #get 'subjects/index'
+  #get 'subjects/show'
+  #get 'subjects/new'
+  #get 'subjects/edit'
+  #get 'subjects/delete'
+  
   get 'fish/newfish'
   root 'demo#index'
 
