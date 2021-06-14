fbf3dd026038a436e43c691e5ca7ed2249b5e3d7
Leum <you@example.com>
2021-06-14 13:54:38 -0500

    Update actions: Edit and Update

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -24,9 +24,17 @@ class SubjectsController < ApplicationController
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def update
+    @subject = Subject.find(params[:id])
+    if @subject.update(subject_params)
+      redirect_to(subject_path(@subject))
+    else
+      render('edit')
+    end
+
   end
 
   def delete
